d5c2d66fee8ef69bcfeec481454aa1ec058420b9
enzolhtt <laheurteenzo4@gmail.com>
2023-10-02 17:22:49 +0200

    Connexion

--- Fulbank/Login.cs
+++ Fulbank/Login.cs
@@ -1,3 +1,6 @@
+using MySql.Data.MySqlClient;
+using System.Security.Cryptography;
+
 namespace Fulbank
 {
     public partial class Login : Form
@@ -5,6 +8,26 @@ namespace Fulbank
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string connstring = "server=172.16.119.21; uid=brochard;pwd=admin;database=FulBank";
+                MySqlConnection conn = new MySqlConnection();
+                conn.ConnectionString = connstring;
+                conn.Open();
+                string sql = "select * from Client;";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            
+
         }
     }
 }
