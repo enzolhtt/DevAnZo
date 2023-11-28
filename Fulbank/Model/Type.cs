623f611254604570db213d12101a880145bcf413
Antonin Brochard <antonin.brochard@gmail.com>
2023-11-28 11:48:10 +0100

    message

---
+++ Fulbank/Model/Type.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fulbank.Model
+{
+    internal class Type
+    {
+        private int _id;
+        private string _typeDeCompte;
+
+        public Type(int unId, string unType) 
+        {
+            _id = unId;
+            _typeDeCompte = unType;
+        }
+
+        public int Id { get { return _id;} }
+        public string TypeDeCompte { get {  return _typeDeCompte;} }
+    }
+}
