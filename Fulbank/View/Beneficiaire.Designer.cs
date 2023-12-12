2d6004cb13375bf4b1aa6343c2eed2a1964707c3
Antonin Brochard <antonin.brochard@gmail.com>
2023-12-12 13:56:46 +0100

    message

---
+++ Fulbank/View/Beneficiaire.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Fulbank.View
+{
+    partial class Beneficiaire
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            img_retour = new PictureBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)img_retour).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // img_retour
+            // 
+            img_retour.Image = Properties.Resources.flecheRetour;
+            img_retour.Location = new Point(33, 30);
+            img_retour.Name = "img_retour";
+            img_retour.Size = new Size(140, 140);
+            img_retour.SizeMode = PictureBoxSizeMode.StretchImage;
+            img_retour.TabIndex = 7;
+            img_retour.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Fulbank;
+            pictureBox1.Location = new Point(620, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(352, 208);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // Beneficiaire
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(128, 194, 236);
+            ClientSize = new Size(984, 961);
+            Controls.Add(pictureBox1);
+            Controls.Add(img_retour);
+            Name = "Beneficiaire";
+            Text = "Beneficiaire";
+            ((System.ComponentModel.ISupportInitialize)img_retour).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox img_retour;
+        private PictureBox pictureBox1;
+    }
+}
